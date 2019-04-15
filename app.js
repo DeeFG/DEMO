77fea190eb2a2dfd99efbd75ceef42490671ae59
DeeFG <45301109+DeeFG@users.noreply.github.com>
2019-04-15 06:54:15 -0400

    add js

--- app.js
+++ app.js
@@ -1,1 +1,55 @@
+// var questions = [];
+// var answers = [];
+var score = 0;
+var totalscore = 0;
+var winnerPhoto= "";
+var losePhoto= "";
+var wins = 0;
+var loss = 0;
 
+
+
+    // on click to click answer question
+    $(".btn") .click(function() {
+        $( ".btn" ).click();
+        console.log("log");
+        // when click on the anwer
+
+        //check to see if anwer is a match to the question
+        //anwer === question loop??? 
+        // if/ else statment
+        // if == true/macth --> DO SOEMTHING
+    });
+
+
+    //     function score() {
+    //         if(checkCorrectAnswer === true)
+    //         $("").attr("#winnerPhoto")
+
+    //         console.log("score")
+    //     }
+
+    //     function checkCorrectAnswer() {
+
+    //         console.log("checkAnwer")
+    //     }
+
+    //     functino reset(){
+    //         console.log("reset")
+    //     }
+    //     function changeQues() {
+
+    //     }
+
+    //     function changeAnsw() {
+
+    //     }
+
+// $("btn.btn-dark:hover").click(function() {
+//     $( this ).slideUp();
+//   });
+
+//   $( " ).hover(function() {
+//     $( this ).fadeOut( 100 );
+//     $( this ).fadeIn( 500 );
+//   });
