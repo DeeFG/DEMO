04074f100a1f3750ed09489ba487d234704adeef
DeeFG <xdeishax@gmail.com>
2019-04-18 15:39:43 -0400

    button values

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -12,7 +12,8 @@ var losePhoto = "";
 var points = 0;
 //var totalQuestions = questions.length;
 var button = $("<button>");
-  
+var userSelect = undefined;
+
 
 var questions = [
     {
@@ -33,15 +34,18 @@ var questions = [
     }
 ];
 
+$("button").click(function () {
+        var text = $( this ).text();
+            $( "input" ).val( text );
+        console.log("hello");
+    });
 
 
 $(document).ready(function () {
     console.log("ready!");
 
-    $("button").click(function () {
-        console.log("hello");
 
-    })
+
     function newBnt() {
         var newBtn = $("<button>");
         $(newBtn).text("clck me");
@@ -55,13 +59,18 @@ $(document).ready(function () {
         $("#box1").text(questions[i].question); // rotate questions
 
         // SHOW--CHOICES 
-        for (var j = 0; j < questions[i].choices[j].length ; j++) {
+        for (var j = 0; j < questions[i].choices[j].length; j++) {
             console.log(questions[i].choices[j]);
-            //$("button").text(questions[i].choices[j]);
+            $("#option").val(questions[i].choices[j]); // rotate questions
+           $("button").text(questions[i].choices[j]);
+           //newbtn.text(questions[i].choices[j]);
+          // ("#option1").append(newbtn);
 
-            for (var k = 0; k < questions[i].answer; k++) {
+
+
+            for (var k = 0; k < questions[i].answer[k]; k++) {
                 /////// --- MATCH ANWERS
-                console.log(questions[i].answer);
+                console.log(questions[i].answer[k]);
             }
             //-------CHANGE QUESTION AND ANWER
             // function changeQues() { }
@@ -71,11 +80,41 @@ $(document).ready(function () {
 
     }
 
-
 });
+
+//--------START GAME-----
+
+
+// ----------SET SCOREING RULES
+function checkCorrectAnswer() {
+    var numIndex = questions[i].answer;
+
+    // if answer is correct
+    if (userSelects === numIndex) {
+        // add to the number of correct answers
+        points++;
+
+        // color the answers green
+        answerContainers[questionNumber].style.color = 'lightgreen';
+    }
+    // if answer is wrong or blank
+    else {
+        // color the answers red
+        answerContainers[questionNumber].style.color = 'red';
+    }
+}
+
+
+//----------RESET
+// functino reset(){
+//     console.log("reset")
+//     new question
+//     new answer
+// }
 
 
 // ------- funtions -------
+///--------ADD TIMER FUNCTION
 function startTimer(duration, display) {
     var timer = duration, minutes, seconds;
     setInterval(function () {
@@ -98,39 +137,4 @@ window.onload = function () {
         display = document.querySelector('#timer');
     startTimer(fiveMinutes, display);
 };
-//--------START GAME-----
-
-///--------ADD TIMER FUNCTION
-//   $(function () {
-//     var timer;
-//     $("#timer").html(timer);
-//     function come() { alert("here"); }
-//     clearInterval(timer);
-//     var timer = setInterval(come, 10000000);
-// });
-
-
-// var timer = setInterval(function () {
-//     if (!score === 0) {
-//         $("timer").text(timer);
-//     }
-//    // clearTimeout(timer);
-// }, 10000);
-
-
-
-//---------CANCEL TIMER WHEN NEEDED 
-
-
-// ----------SET SCOREING RULES
-    function checkCorrectAnswer() {
-        console.log("checkAnwer")  
-    
-    }
-
-//----------RESET
-// functino reset(){
-//     console.log("reset")
-//     new question
-//     new answer
-// }
+// cancel timer-------------
